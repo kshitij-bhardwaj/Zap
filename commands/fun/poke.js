ddb26dc5925a256ba42c9c073c5f84089267ccc9
Adonis <72539206+Adoniswoah@users.noreply.github.com>
2020-12-21 17:23:57 +0530

    Update poke.js

--- commands/fun/poke.js
+++ commands/fun/poke.js
@@ -4,14 +4,11 @@ exports.run = (client, message, args) => {
     let personPoke = message.mentions.members.first();
     let poke = ['https://media.giphy.com/media/PkR8gPgc2mDlrMSgtu/giphy.gif', 'https://media.giphy.com/media/3x5nIjlszTBQs/giphy.gif', 'https://media.giphy.com/media/Vfie0DJryAde8/giphy.gif', 'https://media.giphy.com/media/141SlA8Pv3J7WM/giphy.gif', 'https://media.giphy.com/media/TYAYywTcAb4Iw/giphy.gif', 'https://media.giphy.com/media/MAdHk0h1xIHImwcRa8/giphy.gif', 'https://media.giphy.com/media/1Bh3kuNSGCkPi0oUtI/giphy.gif'];
     let pokerand = poke[Math.floor(Math.random() * poke.length)];
-    let quote = ['Hehehe', 'Lol', 'Giggles', 'It hurts now', 'ahahaha', 'Yeet'];;
-    let quoterand=quote[Math.floor(Math.random() * quote.length)];
     if (!personPoke) {
         let personPoke = "Air";
 
         let embed = new Discord.MessageEmbed()
             .setDescription(`<@${message.author.id}> just poked ${personPoke}!`)
-            .addField(`${quoterand}`)
             .setImage(pokesrand)
             .setColor(`GREEN`);
 
@@ -21,7 +18,6 @@ exports.run = (client, message, args) => {
 
     let embed = new Discord.MessageEmbed()
         .setDescription(`<@${message.author.id}> just poked ${personPoke}!`)
-        .addField(`${quoterand}`)
         .setImage(pokesrand)
         .setColor(`GREEN`);
 
