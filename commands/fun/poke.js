99375294bfc48c33a6f7a7fdfb0fb3712cd0b1b5
Adonis <72539206+Adoniswoah@users.noreply.github.com>
2020-12-21 17:21:37 +0530

    Update poke.js

--- commands/fun/poke.js
+++ commands/fun/poke.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) => {
     let personPoke = message.mentions.members.first();
     let poke = ['https://media.giphy.com/media/PkR8gPgc2mDlrMSgtu/giphy.gif', 'https://media.giphy.com/media/3x5nIjlszTBQs/giphy.gif', 'https://media.giphy.com/media/Vfie0DJryAde8/giphy.gif', 'https://media.giphy.com/media/141SlA8Pv3J7WM/giphy.gif', 'https://media.giphy.com/media/TYAYywTcAb4Iw/giphy.gif', 'https://media.giphy.com/media/MAdHk0h1xIHImwcRa8/giphy.gif', 'https://media.giphy.com/media/1Bh3kuNSGCkPi0oUtI/giphy.gif'];
-    let pokerand = pokerand[Math.floor(Math.random() * pokerand.length)];
+    let pokerand = poke[Math.floor(Math.random() * poke.length)];
     let quote = ['Hehehe', 'Lol', 'Giggles', 'It hurts now', 'ahahaha', 'Yeet'];;
     let quoterand=quote[Math.floor(Math.random() * quote.length)];
     if (!personPoke) {
