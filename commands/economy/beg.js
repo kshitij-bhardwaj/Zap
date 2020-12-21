6b964a112cc7e1b0d096f42d444a20fc9863b3bf
kshitij-bhardwaj <74918061+kshitij-bhardwaj@users.noreply.github.com>
2020-12-21 16:57:48 +0530

    Update beg.js

--- commands/economy/beg.js
+++ commands/economy/beg.js
@@ -1,1 +1,37 @@
-
+const Discord = require('discord.js');
+const db = require("quick.db");
+const ms = require("parse-ms");
+module.exports.run = async (bot, message, args) => {
+    if(!message.content.startsWith('zap'))return;  
+  
+    let user = message.author;
+  
+    let timeout = 600000;
+    let amountrand = [5,10,15,20,25,30];
+    let amount = amountrand[Math.floor(Math.random() * amountrand.length)];
+    let beg = await db.fetch(`beg_${message.guild.id}_${user.id}`);
+  
+    if (beg !== null && timeout - (Date.now() - beg) > 0) {
+      let time = ms(timeout - (Date.now() - beg));
+    
+      let timeEmbed = new Discord.MessageEmbed()
+      .setColor(`RED`)
+      .setDescription(`<a:sgTRIGGERED:790535691145576459> You've already begged recently\n\nBeg again in ${time.minutes}m ${time.seconds}s `);
+      message.channel.send(timeEmbed)
+    } else {
+      let moneyEmbed = new Discord.MessageEmbed()
+    .setColor(`GREEN`)
+    .setDescription(`<a:Coin:790532957705994240>  and received \`${amount}\` coins`);
+    message.channel.send(moneyEmbed)
+    db.add(`money_${message.guild.id}_${user.id}`, amount)
+    db.set(`beg_${message.guild.id}_${user.id}`, Date.now())
+  
+  
+    }
+  };
+  
+  
+  module.exports.help = {
+    name:"beg",
+    aliases: [""]
+  }
