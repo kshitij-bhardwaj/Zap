655a94efc472b0d6931d42425b587846764ed6af
Adonis <72539206+Adoniswoah@users.noreply.github.com>
2020-12-21 21:17:59 +0530

    Update snitch.js

--- commands/fun/snitch.js
+++ commands/fun/snitch.js
@@ -1,22 +1,29 @@
-const { MessageEmbed } = require("discord.js");
-module.exports.run = async (bot, message, args) => {
-    const snitches = client.snipes.get(message.channel.id) || [];
-    const msg = snitches[args[0] - 1 || 0];
-    if (!msg) return message.channel.send(`What do I snitch?`);
-    const Embed = new MessageEmbed()
-      .setAuthor(
-        msg.author.tag,
-        msg.author.displayAvatarURL({ dynamic: true, size: 256 })
-      )
-      .setDescription(msg.content)
-      .setFooter(`Date: ${msg.date} | ${args[0] || 1}/${snipes.length}`);
-    if (msg.attachment) Embed.setImage(msg.attachment);
-    message.channel.send(Embed);
-  },
-};
-module.exports = {
-  name: "snitch",
-  description: "Just snitch a deleted message",
-  usage: "snitch <number>",
-  category: "fun"
+const Discord = require('discord.js')
+
+exports.run = (client, message, args) => {
+       
+    String.prototype.embedify = function() {
+        return new Discord.RichEmbed().setColor(client.config.embedColor).setDescription(this)
+    }
+    
+    let snipe = client.snipeMap.get(message.guild.id)
+    if(!snipe) return message.channel.send('There\'s nothing to snitch'.embedify())
+
+    if(args[0] == 'image') {
+        if(!args[1]) return message.channel.send('Please provide a message to retrieve the image from!'.embedify())
+        let image = snipe[args[1] - 1]
+        if(!image[1]) return message.channel.send('That message does not have an attached (deleted) image!'.embedify())
+        console.log(image[1])
+        return message.channel.send(new Discord.RichEmbed().setColor(client.config.embedColor).setImage(image[1]))
+    }
+
+    let counter = 0
+
+    return message.channel.send(`${snipe.map(msg => `**${++counter} -** ${msg[0].content ? `${msg[0].content}${!msg[1] ? '' : '\n[IMAGE WAS DELETED]'}` : (!msg[1] ? '' : '[IMAGE WAS DELETED]')}\n**Author -** <@${msg[0].author.id}>`).join('\n\n')}`.embedify().addField('NOTE:', `Message appear in order, newest deleted message is \`1.\` ,etc. Only the last five deleted messages are preserved. Messages above a 200 character limit are truncated to fit within the embed. **Due to popular request, one image is now preserved from each message if an image was present!**\nTo view the images, please type \`zap snipe image <number of message to get the image from>\``))
+}
+exports.help = {
+    type: "fun",
+    name: "snitch",
+    description: "The `snitch` command is just the snitch command, idk what it does.",
+    usage: "`zap snitch`"
 }
