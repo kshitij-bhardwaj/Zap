ef3a6fc20547a37da3d8f24af1a05e4ff82f570d
Adonis <72539206+Adoniswoah@users.noreply.github.com>
2020-12-21 15:42:42 +0530

    hmmmm

--- init.js
+++ init.js
@@ -1,30 +1,44 @@
-const Discord = require("discord.js"); 
-const fs = require("fs"); //importing file system module
+const Discord = require("discord.js");
+const fs = require("fs");
+const { resolve } = require("path");
+const walk = require("walk");
 
-const client = new Discord.Client(); //unboxing
+
+
 const config = require("./config.json");
-client.config = config;//accessiblity check
-
+const client = new Discord.Client();
+client.config = config;
 fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
+    if (!file.endsWith(".js")) return;
     const event = require(`./events/${file}`);
     let eventName = file.split(".")[0];
     client.on(eventName, event.bind(null, client));
   });
 });
+client.commands = new Discord.Collection();
+client.categories = new Discord.Collection();
 
-client.commands = new Discord.Collection(); //importing file collections, for organising the files
+const walker = walk.walk("./commands");
+walker.on("file", function (root, stats, next) {
+  if (!stats.name.endsWith(".js")) return;
+  const category = resolve(root).split("\\").slice(-1)[0];
+  if (!client.categories.has(category)) {
+    client.categories.set(category, []);
+  }
 
-fs.readdir("./commands/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load command ${commandName}`);
-    client.commands.set(commandName, props);
-  });
+  let props = require(`${resolve(root)}/${stats.name}`);
+  let commandName = stats.name.split(".")[0];
+  console.log(`Attempting to load command ${commandName}`);
+
+  
+  client.commands.set(commandName, props);
+
+  client.categories.set(category, [
+    ...client.categories.get(category),
+    commandName,
+  ]);
+  next();
 });
-
 client.login(config.token);
