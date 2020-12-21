5f0f058c8a4866921de657c4650ec9a18074afe7
kshitij-bhardwaj <74918061+kshitij-bhardwaj@users.noreply.github.com>
2020-12-21 17:19:06 +0530

    Update beg.js

--- commands/economy/beg.js
+++ commands/economy/beg.js
@@ -11,6 +11,8 @@ module.exports.run = async (bot, message, args) => {
     let amount = amountrand[Math.floor(Math.random() * amountrand.length)];
     let altrusit = ["Taylor Swift", "Carl Johnson", "Joe Biden", "Spongebob", "Goku", "Kira", "Franklin", "OnePunchMan", "Your mother"];
     let altrusitvalue = altrusit[Math.floor(Math.random() * altrusit.length)];
+    let blocked= ["**Beg too much and the economy will crash**", "**Why don't you work? Huh?**", "**IMF ran out of money!**"];
+    let blockedvalue = blocked[Math.floor(Math.random() * blocked.length)];
     let beg = await db.fetch(`beg_${message.guild.id}_${user.id}`);
   
     if (beg !== null && timeout - (Date.now() - beg) > 0) {
@@ -18,7 +20,7 @@ module.exports.run = async (bot, message, args) => {
     
       let timeEmbed = new Discord.MessageEmbed()
       .setColor(`RED`)
-      .setDescription(`<a:sgTRIGGERED:790535691145576459> You've already begged recently\n\nBeg again in ${time.minutes}m ${time.seconds}s `);
+      .setDescription(`<a:sgTRIGGERED:790535691145576459> ${blockedvalue} \n\nBeg again in ${time.minutes}m ${time.seconds}s `);
       message.channel.send(timeEmbed)
     } else {
       let moneyEmbed = new Discord.MessageEmbed()
