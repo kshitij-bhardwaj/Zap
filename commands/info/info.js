e7a718dad9d54ef39776a3b4f0c36ad48d135284
Adonis <72539206+Adoniswoah@users.noreply.github.com>
2020-12-22 16:10:46 +0530

    Update info.js

--- commands/info/info.js
+++ commands/info/info.js
@@ -3,6 +3,13 @@ const { MessageEmbed } = require('discord.js')
 const { Menu } = require('discord.js-menu');
 
 module.exports.run =(client, message, args) => {
+    let totalSeconds = (client.uptime / 1000);
+let days = Math.floor(totalSeconds / 86400);
+totalSeconds %= 86400;
+let hours = Math.floor(totalSeconds / 3600);
+totalSeconds %= 3600;
+let minutes = Math.floor(totalSeconds / 60);
+let seconds = Math.floor(totalSeconds % 60);
     
     let helpMenu = new Menu(message.channel, message.author.id, [
             // Each object in this array is a unique page.
@@ -12,7 +19,7 @@ module.exports.run =(client, message, args) => {
               
                 content: new MessageEmbed({
                     title: '**Bot Information**',
-                    description: `**Serveing** \n Number of Servers:${client.guilds.cache.size} \n Number of Channels:${client.channels.cache.size} \n Number of members: ${client.users.cache.size} \n **Features** \n \`Ram usage\`::${((process.memoryUsage().heapUsed / 1024) / 1024).toFixed(2)} MB \n \`Library\`::Discord JS v${Discord.version} \n \`Host\`::${os.hostname} \n \`OS\`::${os.platform} ${os.release} \`version\`::1.0.0 -b \n`
+                    description: `**Serveing** \n ${client.guids.cache.size} servers with ${client.channels.cache.size} channels and ${client.users.cache.size} users. \n **Uptime** \n ${days}:${hours}:${minutes}:${seconds}\n **Ping** \n Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms\n This bot was made by Tensor Calculus Mistakes#1729 and Adonis#5927, deemed to increas the expierence of users on discord to a better level! `
                 }),
                 reactions: {
                     '⏹': 'delete',
