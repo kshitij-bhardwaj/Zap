965d6190cbaf785c54f5c9eb3337515641270f3d
kshitij-bhardwaj <74918061+kshitij-bhardwaj@users.noreply.github.com>
2020-12-22 17:18:59 +0530

    Add files via upload

---
+++ commands/economy/withdraw.js
@@ -0,0 +1,61 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+const ms = require("parse-ms");
+
+module.exports.run = async (bot, message, args) => {
+  if(!message.content.startsWith('zap'))return;  
+
+  let user = message.author;
+
+  let member = db.fetch(`money_${message.guild.id}_${user.id}`)
+  let member2 = db.fetch(`bank_${message.guild.id}_${user.id}`)
+
+  if (args[0] == 'all') {
+    let money = await db.fetch(`bank_${message.guild.id}_${user.id}`)
+    
+    db.subtract(`bank_${message.guild.id}_${user.id}`, money)
+    db.add(`money_${message.guild.id}_${user.id}`, money)
+    let embed5 = new Discord.MessageEmbed()
+  .setColor(`GREEN`)
+  .setDescription(`<a:My_best_verified:790894580643397653> You have withdrawn all your coins from your bank`);
+  message.channel.send(embed5)
+  
+  } else {
+
+  let embed2 = new Discord.MessageEmbed()
+  .setColor(`RED`)
+  .setDescription(`<a:no:790889592395792404> Specify an amount to withdraw`);
+  
+  if (!args[0]) {
+      return message.channel.send(embed2)
+  }
+  let embed3 = new Discord.MessageEmbed()
+  .setColor(`RED`)
+  .setDescription(`<a:no:790889592395792404> You can't withdraw negative money`);
+
+  if (message.content.includes('-')) { 
+      return message.channel.send(embed3)
+  }
+  let embed4 = new Discord.MessageEmbed()
+  .setColor(`RED`)
+  .setDescription(`<a:no:790889592395792404> You don't have that much money in the bank`);
+
+  if (member2 < args[0]) {
+      return message.channel.send(embed4)
+  }
+
+  let embed5 = new Discord.MessageEmbed()
+  .setColor(`GREEN`)
+  .setDescription(`<a:My_best_verified:790894580643397653> You have withdrawn ${args[0]} coins from your bank`);
+
+  message.channel.send(embed5)
+  db.subtract(`bank_${message.guild.id}_${user.id}`, args[0])
+  db.add(`money_${message.guild.id}_${user.id}`, args[0])
+  }
+}
+
+
+module.exports.help = {
+  name:"withdraw",
+  aliases: ["wd"]
+}
