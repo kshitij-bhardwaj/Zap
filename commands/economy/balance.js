c66776546273df301b8a644e483642f9181ebfb6
kshitij-bhardwaj <74918061+kshitij-bhardwaj@users.noreply.github.com>
2020-12-22 17:16:27 +0530

    Add files via upload

---
+++ commands/economy/balance.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+
+module.exports.run = async (bot, message, args, utils) => {
+  if(!message.content.startsWith('zap'))return;  
+
+  let user = message.mentions.members.first() || message.author;
+
+  let bal = db.fetch(`money_${message.guild.id}_${user.id}`)
+
+  if (bal === null) bal = 0;
+
+  let bank = await db.fetch(`bank_${message.guild.id}_${user.id}`)
+  if (bank === null) bank = 0;
+
+  let moneyEmbed = new Discord.MessageEmbed()
+  .setColor(`GREEN`)
+  .setDescription(`<a:My_best_verified:790894580643397653>**${user}'s Balance**\n\nWallet: ${bal}\nBank: ${bank}`);
+  message.channel.send(moneyEmbed)
+};
+
+module.exports.help = {
+  name:"balance",
+  aliases: ["bal"]
+}
