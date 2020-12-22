aa48bee284294075b10d3d82eebdbd3fb0dbe4fc
Adonis <72539206+Adoniswoah@users.noreply.github.com>
2020-12-22 11:51:31 +0530

    Create randomcat.js

---
+++ commands/fun/randomcat.js
@@ -0,0 +1,21 @@
+const request = require('request'); 
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+        request('http://edgecats.net/random', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                    let embed = new Discord.RichEmbed()
+                    .setImage(body)
+                    .setColor("#00ff00")
+                    .setTitle("A Cat")
+                              
+                   message.channel.send(embed)  
+            }
+        });
+    }
+
+module.exports.help = {
+  name:"cat",
+  usage:"zap randomcat",
+  description"Brings up a random cat from no where"
+}
