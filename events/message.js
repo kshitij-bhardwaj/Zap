f9919858a3187db0155ff7c0b4003ff21145cf61
Adonis <72539206+Adoniswoah@users.noreply.github.com>
2020-12-25 12:02:30 +0530

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,3 +1,5 @@
+const {array} = require('../profanity.json');
+const Discord = require('discord.js');
 module.exports = (client, message) => {
   
   if (message.author.bot) return;
@@ -10,7 +12,18 @@ module.exports = (client, message) => {
 
 
   const cmd = client.commands.get(command);
+  var censorList = array;
+  var myMsg = message.content;
+  embed = new Discord.MessageEmbed()
+  .setDescription(`Your message in ${message.guild.name} was deleted due to the following reason: \n Contains bad word ${c} \n If you think this is a error or glitch please report this to the staff. \n\n Here's your message \n ${myMsg}`);
 
+  
+for (c in censorList) {
+        if (myMsg.includes(censorList[c])){
+            message.delete().catch(console.error);
+            message.author.send(embed).catch(console.error);
+        }
+    };
 
   if (!cmd) return;
 
