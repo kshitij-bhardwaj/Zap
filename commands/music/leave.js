9571015422bf2d170164eee31a6862e0b61d9097
Adonis <72539206+Adoniswoah@users.noreply.github.com>
2020-12-30 15:38:55 +0530

    Update leave.js

--- commands/music/leave.js
+++ commands/music/leave.js
@@ -1,18 +1,22 @@
-const Discord = require('discord.js');
-module.exports.run = async(client, message) =>{
-    //name: 'leave',
-    //description: 'stop the bot and leave the channel',
+const Discord = require("discord.js")
+const fs = require("fs")
+
+module.exports.run = async (client, message, args) => {
+  
+  if(!message.member.voice.channel) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | You must be in a voice channel!` }})
     
-        const voiceChannel = message.member.voice.channel;
-        let Embed1= new Discord.MessageEmbed()
-                        .setColor(`RED`)
-                        .setDescription(`<a:no:791738978180399114>You need to be in a voice channel to stop the music!`);
-        if(!voiceChannel) return message.channel.send(Embed1);
-        await voiceChannel.leave();
-        let Embed= new Discord.MessageEmbed()
-                        .setColor(`GREEN`)
-                        .setDescription('***Leaving channel***');
-        await message.channel.send(Embed)
- 
-    
+  if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | You are not in my voice channel!`}});
+  
+  if(!client.player.isPlaying(message.guild.id)) return message.channel.send({embed: {color: client.colors.error, description: `${client.emotes.error} | There is nothing playing!` }})
+  
+  let song = await client.player.stop(message.guild.id);
+
+  message.channel.send({embed: {color: client.colors.success, description: `${client.emotes.stop} | Disconnected!` }})
+
+
+}
+
+module.exports.config = {
+  name: "leave",
+  aliases: []
 }
