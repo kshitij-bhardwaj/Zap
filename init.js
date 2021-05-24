da1dc5c29aee8e679e76c88e4581364878873ffc
kshitij-bhardwaj <74918061+kshitij-bhardwaj@users.noreply.github.com>
2021-05-24 14:35:46 +0530

    Add files via upload

--- init.js
+++ init.js
@@ -1,15 +1,15 @@
+require('dotenv').config();
 const Discord = require("discord.js");
+const DisTube = require("distube")
 const fs = require("fs");
 const { resolve } = require("path");
 const walk = require("walk");
-
-const { Player } = require("discord-player");
-const player = new Player(client);
-
-client.player = player;
-
 const config = require("./config.json");
 const client = new Discord.Client();
+const DBL = require("dblapi.js");
+const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc5MDQ4MjE0NDY2NTQwMzQyMiIsImJvdCI6dHJ1ZSwiaWF0IjoxNjEwMzgwNjI1fQ.5ZOzZ7rSOwE3ziHSGLfj1SjQGt6o8p-wEOFXdpNdWbA', client);
+
+
 
 client.config = config;
 fs.readdir("./events/", (err, files) => {
@@ -22,9 +22,12 @@ fs.readdir("./events/", (err, files) => {
   });
 });
 client.commands = new Discord.Collection();
+client.distube = new DisTube(client, { searchSongs: true, emitNewSongOnly: true, leaveOnFinish: true })
 client.aliases = new Discord.Collection();
 client.categories = new Discord.Collection();
 client.snipes = new Discord.Collection();
+client.games = new Map();
+client.queue2 = new Map();
 
 const walker = walk.walk("./commands");
 walker.on("file", function (root, stats, next) {
@@ -47,4 +50,42 @@ walker.on("file", function (root, stats, next) {
   ]);
   next();
 });
-client.login(config.token);
+const status = queue => `Volume: \`${queue.volume}%\` | Filter: \`${queue.filter || "Off"}\` | Loop: \`${queue.repeatMode ? queue.repeatMode === 2 ? "All Queue" : "This Song" : "Off"}\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``
+
+client.distube
+    .on("playSong", (message, queue, song) => {
+      const embed = new Discord.MessageEmbed().setTitle(`${message.guild.name}'s Music Queue`)
+                                              .addField(`🎶Playing:`, `${song.name}`)
+                                              .addField(`⏰Duration:`,`${song.formattedDuration}`)
+                                              .setDescription(`${status(queue)}`)
+                                              .setThumbnail(`${song.thumbnail}`)
+                                              .setFooter(`Requested by: ${song.user.user}`, song.user.displayAvatarURL())
+                                              .setColor(`GREEN`)
+      message.channel.send(embed);
+    })
+    .on("addSong", (message, queue, song) =>{ 
+      const embed = new Discord.MessageEmbed().setTitle(`${message.guild.name}'s Music Queue`)
+                                              .addField(`Queue:`, `${song.name}`)
+                                              .addField(`Duration:`,`${song.formattedDuration}`)
+                                              .setDescription(`${status(queue)}`)
+                                              .setThumbnail(`${song.thumbnail}`)
+                                              .setFooter(`Requested by: ${song.user.user}`, song.user.displayAvatarURL())
+                                              .setColor(`GREEN`)
+      message.channel.send(embed);
+    })
+    /* .on("playList", (message, queue, playlist, song) => message.channel.send(
+        `Play \`${playlist.title}\` playlist (${playlist.total_items} songs).\nRequested by: ${song.user}\nNow playing \`${song.name}\` - \`${song.formattedDuration}\`\n${status(queue)}`
+    ))
+    .on("addList", (message, queue, playlist) => message.channel.send(
+        `Added \`${playlist.title}\` playlist (${playlist.total_items} songs) to queue\n${status(queue)}`
+    )) */
+    // DisTubeOptions.searchSongs = true
+    .on("searchResult", (message, result) => {
+        let i = 0
+        message.channel.send({embed:{color:`GREEN`, description:`**Choose an option from below**\n${result.slice(0,5).map(song => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")}\n*Enter anything else or wait 60 seconds to cancel*`}})
+    })
+    // DisTubeOptions.searchSongs = true
+    .on("searchCancel", message => message.channel.send(` Searching canceled`))
+    .on("finish", message => {message.channel.send("No more song in queue."), setTimeout(client.distube.stop(message), 600000)})
+    .on("error", (message, err) => message.channel.send(` An error encountered: ${err}`))
+client.login(process.env.DISCORD_TOKEN);
